58ef2c33490a73f38e321b934b27dd571639a808
Domas <domas.masiulis@gmail.com>
2025-09-28 20:56:26 +0100

    max

--- practise/5. argo-rollback/max/deploy.ps1
+++ practise/5. argo-rollback/max/deploy.ps1
@@ -2,9 +2,9 @@ param(
     [Parameter(Mandatory = $true)]
     [string]$RepositoryUrl,
 
-    [string]$Revision = "main",
-    [string]$AppName = "rollback-blue-to-purple-domas",
-    [string]$Namespace = "blue-app-domas"
+  [string]$Revision = "main",
+  [string]$AppName = "rollback-blue-to-purple-max",
+  [string]$Namespace = "blue-app-max"
 )
 
 $ErrorActionPreference = "Stop"
@@ -24,7 +24,7 @@ spec:
   source:
     repoURL: $RepositoryUrl
     targetRevision: $Revision
-    path: practise/5. argo-rollback/domas/k8s
+  path: practise/5. argo-rollback/max/k8s
   destination:
     server: https://kubernetes.default.svc
     namespace: $Namespace
